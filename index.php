3ce78abb3e6b979091cb912b64e29088e736174c
BTracas <benjamin.tracas@limayrac.fr>
2022-09-29 15:03:06 +0200

    Accueil basique liens vers autres pages

--- index.php
+++ index.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FREDI</title>
+    <link rel="stylesheet" href="css/index.css">
+    <title>FREDI accueil</title>
 </head>
 <body>
+    <header>
+    <button onclick="window.location.href = 'index.php';">Accueil</button>
+    <button class="droite" onclick="window.location.href = 'connexion.php';">Connexion</button>
+    <button class="droite" onclick="window.location.href = 'inscription.php';">Inscription</button>
+
+    </header>
     <h1>FREDI : Le site de l'avenir du sport</h1>
+
+    <!--ajouter des images ou d'autres choses-->
+    
 </body>
 </html>
