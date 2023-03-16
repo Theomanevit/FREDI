6cabf350c94fdc633d0b7f3b28f15be6858335e0
theomanevit <theo.manevit@limayrac.fr>
2023-03-16 16:40:17 +0100

    css

--- fomulaire_modif.php
+++ fomulaire_modif.php
@@ -15,16 +15,45 @@ require('backend/CRUD_ligne_frais/modif_ligne.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>modifier une ligne de frais</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/connexion.css">
+    <link rel="stylesheet" href="css/tableau.css">
 </head>
 
 <body>
     <?php include('header.php') ?>
     <h1>modifier une ligne de frais</h1>
-    <form action="<?php echo $_SERVER['PHP_SELF'].'?id_nfrais='.$id_nfrais.'';?>" method="post">
-    <div class="field padding-bottom--24">
+    <div class="formbg-outer">
+        <div class="formbg">
+            <div class="formbg-inner padding-horizontal--48">
+                <form action="<?php echo $_SERVER['PHP_SELF'].'?id_nfrais='.$id_nfrais.'';?>" method="post">
+                    <div class="field padding-bottom--24">
+                        <label>Libellé du déplacement</label>
+                        <input type="text" name="lib_deplace" id="lib_deplace" value="<?php echo $lib_deplace ?>" required>
+                    </div>
+                    <div class="field padding-bottom--24">
+                        <label>Date du deplacement</label>
+                        <input type="date" name="date_deplace" id="date_deplace" value="<?php echo $date_deplace ?>" required>
+                    </div>
+                    <div class="field padding-bottom--24">
+                        <label>Frais de péage</label>
+                        <input type="text" name="frais_peage" id="frais_peage" value="<?php echo $frais_peage ?>" required>
+                    </div>
+                    <div class="field padding-bottom--24">
+                        <label>Frais de repas</label>
+                        <input type="text" name="frais_repas" id="frais_repas" value="<?php echo $frais_repas ?>" required>
+                    </div>
+                    <div class="field padding-bottom--24">
+                        <label>Frais d'hebergement</label>
+                        <input type="text" name="frais_heber" id="frais_heber" value="<?php echo $frais_heber ?>" required>
+                    </div>
+                    <div class="field padding-bottom--24">
+                        <label>Nombre de kilomètre</label>
+                        <input type="text" name="nb_km" id="nb_km" value="<?php echo $nb_km ?>" required>
+                    </div>
+                    <div class="field padding-bottom--24">
                         <label>Motif</label>
                         <select name="id_motif" id="id_motif" value=<?php echo $id_motif ?> required>
+                            <option value="">Choisissez un motif</option>
                             <option value="1">Réunion</option>
                             <option value="2">Compétition régionale</option>
                             <option value="3">Compétition nationale</option>
@@ -36,16 +65,17 @@ require('backend/CRUD_ligne_frais/modif_ligne.php');
                             <option value="9">Formation</option>
                         </select>
                     </div>
-        <p>Trajet <br /><input name="lib_deplace" id="lib_deplace" type="text" value="<?php echo $lib_deplace ?>" required /></p>
-        <p>Date du deplacement<br /><input name="date_deplace" id="date_deplace" type="date" value="<?php echo $date_deplace ?>" required /></p>
-        <p>Frais de péage<br /><input name="frais_peage" id="frais_peage" type="text" value="<?php echo $frais_peage ?>" required /></p>
-        <p>Frais de repas<br /><input name="frais_repas" id="frais_repas" type="text" value="<?php echo $frais_repas ?>" required /></p>
-        <p>Frais d'hebergement<br /><input name="frais_heber" id="frais_heber" type="text" value="<?php echo $frais_heber ?>" required /></p>
-        <p>Nombre de kilomètres<br /><input name="nb_km" id="nb_km" type="text" value="<?php echo $nb_km ?>" required /></p>
-        <div><input name="id_nfrais" id="id_nfrais" type="hidden" value="<?php echo $id_nfrais; ?>" /></div>
-        <div><input name="id_adherant" id="id_adherant" type="hidden" value="<?php echo $id_adherant; ?>" /></div>
-        <div><input name="id_lfrais" id="id_lfrais" type="hidden" value="<?php echo $id_lfrais; ?>" /></div>
-        <p><input type="submit" name="submit" value="Envoyer" />&nbsp;<input type="reset" value="Réinitialiser" /></p>
-    </form>
+                    <div>
+                        <div><input name="id_nfrais" id="id_nfrais" type="hidden" value="<?php echo $id_nfrais; ?>" /></div>
+                        <div><input name="id_adherant" id="id_adherant" type="hidden" value="<?php echo $id_adherant; ?>" /></div>
+                        <div><input name="id_lfrais" id="id_lfrais" type="hidden" value="<?php echo $id_lfrais; ?>" /></div>
+                    </div>
+                    <div class="field padding-bottom--24">
+                        <input type="submit" name="submit" value="Validée" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
