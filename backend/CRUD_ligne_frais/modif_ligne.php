6957db410362a6815b57bd2cf441ca5acb834886
BTracas <benjamin.tracas@limayrac.fr>
2023-03-16 14:49:24 +0100

    fix crud modif et suppr affichage formulaire

--- backend/CRUD_ligne_frais/modif_ligne.php
+++ backend/CRUD_ligne_frais/modif_ligne.php
@@ -116,7 +116,7 @@ if ($submit) {
     header("location: note_util.php");
 } else {
     try {
-        $sql = "select date_deplace,lib_motif, lignefrais.id_motif, lib_deplace,nb_km,montant_fisc,frais_peage,frais_repas,frais_heber FROM lignefrais , notefrais , periodefiscale , motifdeplacement where motifdeplacement.id_motif = lignefrais.id_motif and lignefrais.id_nfrais = notefrais.id_nfrais and notefrais.id_fisc = periodefiscale.id_fisc";
+        $sql = "select date_deplace,lib_motif, lignefrais.id_motif, lib_deplace,nb_km,montant_fisc,frais_peage,frais_repas,frais_heber FROM lignefrais , notefrais , periodefiscale , motifdeplacement where motifdeplacement.id_motif = lignefrais.id_motif and lignefrais.id_nfrais = notefrais.id_nfrais and notefrais.id_fisc = periodefiscale.id_fisc and id_lfrais =:id_lfrais";
         $sth = $dbh->prepare($sql);
         $sth->execute(array(":id_lfrais" => $id_lfrais));
         $row = $sth->fetch(PDO::FETCH_ASSOC);
